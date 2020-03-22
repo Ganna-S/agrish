20d2896fb0dea987d255b674a38f2f8ed5afd258
Ganna-S <gannashutenko@gmail.com>
2020-03-22 21:05:32 +0200

    animation updated

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,22 @@ $(function () {
         dots: true,
         arrows: false,
     });
+
+    new WOW().init();
+
+    var Animation = function () {
+        var hFrom = $(window).scrollTop();
+        var hTo = $(window).scrollTop() + ($(window).height() / 2);
+        if ($(".progress__inner").offset().top > hFrom && $(".progress__inner").offset().top < hTo) {
+            $('.number').delay(500).spincrement({
+                thousandSeparator: "",
+                duration: 4000
+            });
+        }
+    };
+    $(window).scroll(function () {
+        Animation();
+    });
+    Animation();
 });
 
